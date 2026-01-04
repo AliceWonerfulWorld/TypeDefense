46ed4d9f3a045b8bd9095897e6cddc4b92ddffaa
AliceWonerfulWorld <gomaazarasi1789@gmail.com>
2026-01-04 23:42:53 +0900

    feat: インポートの追加

--- src/TypeDefense.java
+++ src/TypeDefense.java
@@ -14,6 +14,7 @@ import java.util.List;
 import java.util.Timer;
 import java.util.TimerTask;
 import java.util.Random;
+import java.fx.scene.input.Image;
 
 
 public class TypeDefense extends Application {
