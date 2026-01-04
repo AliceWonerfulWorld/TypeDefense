86a3e0f38a060e69826248be77200b531e61ca4b
AliceWonerfulWorld <gomaazarasi1789@gmail.com>
2026-01-04 23:15:02 +0900

    feat: 必要なライブラリの追加

--- src/TypeDefense.java
+++ src/TypeDefense.java
@@ -13,6 +13,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Timer;
 import java.util.TimerTask;
+import java.util.Random;
 
 
 public class TypeDefense extends Application {
