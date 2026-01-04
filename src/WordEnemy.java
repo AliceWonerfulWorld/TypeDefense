13d6c94eca8af2584b7ed2c059d53b822fddd474
AliceWonerfulWorld <gomaazarasi1789@gmail.com>
2026-01-04 21:45:56 +0900

    feat: WordEnemyクラスを追加

--- src/WordEnemy.java
+++ src/WordEnemy.java
@@ -1,3 +1,20 @@
 public class WordEnemy {
-    
+
+    // 敵が持っているデータ
+    public String word; // 表示される単語 
+    public double x;    // 横の位置
+    public double y;    // 縦の位置
+
+    // コンストラクタ
+    // new WordEnemy("test", 100, 0)のように書くと、ここの部分が動く
+    public WordEnemy(String word, double x, double y) {
+        this.word = word;
+        this.x = x;
+        this.y = y;
+    }
+
+    // 敵の動きを決めるメソッド
+    public void move(double speed) {
+        this.y = this.y + speed; // y座標にスピードの分を足す(画面下方向へ進む)
+    }
 }
