b786275889efe5c69b33a58ec0308ccaef7f91a4
AliceWonerfulWorld <gomaazarasi1789@gmail.com>
2026-01-04 22:38:53 +0900

    feat: 必要なライブラリを追加

--- src/TypeDefense.java
+++ src/TypeDefense.java
@@ -7,6 +7,8 @@ import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.paint.Color;
 import javafx.scene.text.Font;
+import javafx.scene.input.KeyEvent;
+import javafx.scene.input.KeyCode;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Timer;
@@ -22,7 +24,6 @@ public class TypeDefense extends Application {
     private Canvas canvas;       // 描画領域
     private GraphicsContext gc;  // 描画用の筆
     private Timer timer;         // タイマー
-
     private List<WordEnemy> enemies = new ArrayList<>(); // 敵の管理用のリスト
 
     public static void main(String[] args) {
