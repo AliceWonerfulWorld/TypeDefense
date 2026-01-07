e6d84c60ba54d28feb5c3ffe4559e6e097bff637
AliceWonerfulWorld <gomaazarasi1789@gmail.com>
2026-01-07 15:26:47 +0900

    feat: GameConstantsクラスを追加し、初期サイズと単語リストを定義

---
+++ src/GameConstants.java
@@ -0,0 +1,12 @@
+public class GameConstants {
+    // 画面の初期サイズ
+    public static final int INITIAL_WIDTH = 600;
+    public static final int INITIAL_HEIGHT = 550;
+
+    // 単語リスト
+    public static final String[] WORDS = {
+        "JAVA", "CLASS", "OBJECT", "METHOD", "PUBLIC", "STATIC",
+        "VOID", "RETURN", "IMPORT", "JAVAFX", "CANVAS", "NODE",
+        "STRING", "INTEGER", "DOUBLE", "BOOLEAN", "SYSTEM", "OUT"
+    }
+}
